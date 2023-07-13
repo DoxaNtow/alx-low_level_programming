90eb95361d69c25960578c2fe558bfcbe0156b36
DoxaNtow <dianasenakey@gmail.com>
2023-07-13 16:31:21 -0700

    ouput with printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,8 @@
 * main - using the printf function to print out
 * Return: 0
 */
-int main(void){
-	int printf("with proper grammar, but the outcome is a piece of art,");
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
