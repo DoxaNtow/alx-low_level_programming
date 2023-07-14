7d513862cce9a9c26b32a0ad5ba9b5e146b7be47
DoxaNtow <dianasenakey@gmail.com>
2023-07-14 15:27:35 -0700

    base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the last digit
+ *Return: 0
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 10)
+	{
+		printf("%d", num);
+		num++;
+	}
+	return (0);
+}
+
