35e95cef794d78933da9ab6a8fa7e31cd368a41b
DoxaNtow <dianasenakey@gmail.com>
2023-07-14 14:41:48 -0700

    print alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the last digit
+ *Return: 0
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
+
