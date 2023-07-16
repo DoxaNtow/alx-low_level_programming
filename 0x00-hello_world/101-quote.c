c9481131ace30f5f37d0278be10a30bc3dd29512
DoxaNtow <dianasenakey@gmail.com>
2023-07-16 06:37:32 -0700

    using fput instead of printf and puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 /**
  * main - A c program that prints a line to the standard error
  *
- *Reture 1
+ *Return: 1
  */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1); 
+	return (1);
 }
