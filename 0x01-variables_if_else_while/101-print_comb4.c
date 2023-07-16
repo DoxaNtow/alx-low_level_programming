2458eea28d28f1c900938ec3e0baf7a53d90e981
DoxaNtow <dianasenakey@gmail.com>
2023-07-16 13:09:24 -0700

    prints all possible different combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,22 +11,18 @@ int main(void)
 	int j = '0';
 	int k = '0';
 
-	while (i <= '9')
+	while (i <= '7')
 	{
-		while (j <= '9')
+		while (j <= '8')
 		{
 			while (k <= '9')
 			{
-				if (!(((i > j) || i == j) && ((j > k) || j == k)))
+				if (i < j && j < k)
 				{
 					putchar(i);
 					putchar(j);
 					putchar(k);
 					if (i == '7' && j == '8' && k == '9')
-					{
-						putchar('\n');
-					}
-					else
 					{
 						putchar(',');
 						putchar(' ');
@@ -34,10 +30,12 @@ int main(void)
 				}
 				k++;
 			}
+			k = '0';
 			j++;
 		}
 		j = '0';
 		i++;
 	}
+	putchar('\n');
 	return (0);
 }
