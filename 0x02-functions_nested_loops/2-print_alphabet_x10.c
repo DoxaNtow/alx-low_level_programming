6d352d9929e6c203a34aa30efd132bada53187b2
DoxaNtow <dianasenakey@gmail.com>
2023-07-18 07:54:18 -0700

    print alphabets 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Make the alphabet x10 times
+ *Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char letter = 'a';
+	int i = 0;
+
+	while (i < 10)
+	{
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+	i++;
+	_putchar('\n');
+	}
+
+}
