70ed875cccbc8567d9a6af8acad04fc1b09562fb
DoxaNtow <dianasenakey@gmail.com>
2023-07-21 17:27:57 -0700

    multiple two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiples two numbers thus a and b
+ * @a: - first number
+ * @b: - second number
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	int multi;
+
+	multi = a * b;
+	return (multi);
+}
