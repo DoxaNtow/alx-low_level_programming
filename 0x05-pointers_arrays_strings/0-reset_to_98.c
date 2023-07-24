69953961aa1e9b2cd3b150496ef2be4f4602e1df
DoxaNtow <dianasenakey@gmail.com>
2023-07-24 11:38:39 -0700

    swap to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - function that takes a pointer to an int and changes to 98
+ * @n: - the parameter to be updated to 98
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+}
