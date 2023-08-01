8043704f412d104acddbd453a6e87cad2958461b
DoxaNtow <dianasenakey@gmail.com>
2023-08-01 11:27:45 -0700

    printing cheesboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_chessboard - Entry point
+ * @a: array
+ * Return: Always 0 (Success)
+ */
+void print_chessboard(char (*a)[8])
+{
+	int x = 0;
+	int y = 0;
+
+	while (x < 8)
+	{
+		while (y < 8)
+		{
+			_putchar(a[x][y]);
+			y++;
+		}
+		_putchar('\n');
+		x++;
+		y = 0;
+	}
+}
+
