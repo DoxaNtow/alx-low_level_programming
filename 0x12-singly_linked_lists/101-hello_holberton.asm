bebcf376f86b2b786fee1815d37af2dbf000d347
DoxaNtow <dianasenakey@gmail.com>
2023-08-25 16:56:45 -0700

    print string in assembly code

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,14 @@
+section .data
+	format: db `Hello, Holberton\n`,0
+
+section .text
+	global main
+	extern printf
+
+main:
+	mov   edi, format
+	xor   eax, eax
+	call  printf
+	xor   eax, eax  ; Set eax to 0
+	ret
+
