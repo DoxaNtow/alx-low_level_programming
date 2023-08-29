b055f6709576d89a742723ba77437f220140e924
DoxaNtow <dianasenakey@gmail.com>
2023-08-29 12:55:46 -0700

    free linked list2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,25 +1,25 @@
 #include "lists.h"
 
 /**
- * free_listint2 - deallocates a linked list and sets the head to NULL
+ * free_listint2 - frees a linked list and sets the head to NULL
  * @head: pointer to the listint_t list to be deallocated
  */
 void free_listint2(listint_t **head)
 {
 	listint_t *current = *head;
-	listint_t *next;
 
-	if (head == NULL || *head == NULL)
+	if (head == NULL)
 		return;
-
 
 	while (current != NULL)
 	{
-		next = current->next;
+		listint_t *next = current->next;
+
 		free(current);
 		current = next;
 	}
 
 	*head = NULL;
 }
+
 
