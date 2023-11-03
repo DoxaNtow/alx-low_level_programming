fa8ecc8b32b19d5904ad9d8d4741691fbd062089
DoxaNtow <dianasenakey@gmail.com>
2023-11-03 13:43:18 -0700

    retrieves value associated with key

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,33 +1,32 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_get - Retrieves the value associated with a key in the hash table.
+ * hash_table_get - Retrieves the value associated with a key.
  * @ht: A pointer to the hash table.
  * @key: The key to retrieve the associated value.
  *
- * Return: The value associated with the key, or NULL if the key does not exist.
+ * Return: value associated with the key, or NULL.
  */
-
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    unsigned long int idx = 0;   /* Index within the hash table */
-    hash_node_t  *node;          /* Current node in the hash table */
+	unsigned long int idx = 0; /* Index within the hash table */
+	hash_node_t *node;         /* Current node in the hash table */
 
-    /* Check for invalid inputs */
-    if (!ht || !key || !*key)
-        return (NULL);
+	/* Check for invalid inputs */
+	if (!ht || !key || !*key)
+		return (NULL);
 
-    /* Calculate the index for the key */
-    idx = key_index((const unsigned char *)key, ht->size);
-    node = ht->array[idx];
+	/* Calculate the index for the key */
+	idx = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[idx];
 
-    /* Search for the key in the bucket */
-    while (node)
-    {
-        if (!strcmp(key, node->key))
-            return (node->value); /* Return the associated value */
-        node = node->next;
-    }
+	/* Search for the key in the bucket */
+	while (node)
+	{
+		if (!strcmp(key, node->key))
+			return (node->value); /* Return the associated value */
+		node = node->next;
+	}
 
-    return (NULL); /* Key not found, return NULL */
+	return (NULL); /* Key not found, return NULL */
 }
